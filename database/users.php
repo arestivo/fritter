6ee465da3f24e7af0555f2963cdd29ac15774ab7
André Restivo <andre.restivo@gmail.com>
2017-11-11 00:49:56 +0000

    Update users.php

--- database/users.php
+++ database/users.php
@@ -1,6 +1,6 @@
 <?php
 
-  function createUser($username, $realname, $password) {
+  function createUser($realname, $username, $password) {
     global $conn;
     $stmt = $conn->prepare('INSERT INTO users VALUES (?, ?, ?)');
     $stmt->execute(array($username, $realname, sha1($password)));
